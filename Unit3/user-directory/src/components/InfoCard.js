a17e7737d241708a7cccb0c337a70c454f9e766b
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-06-05 22:02:03 -0700

    next and previous buttons

--- Unit3/user-directory/src/components/InfoCard.js
+++ Unit3/user-directory/src/components/InfoCard.js
@@ -2,11 +2,30 @@ import React, {useState} from 'react';
 import data from '../data'
 
 const InfoCard = () => {
-    const {id, name, city, country, employer, title, favoriteMovies} = data[0];
+  const [index, setIndex] = useState(0);
+
+  const next = () => {
+    setIndex(index + 1);
+  }
+
+  const previous = () => {
+    setIndex(index - 1);
+  }
 
   return (
     <div>
-        <h1>{id}</h1>
+        <h1>{data[index].id}</h1>
+        <h1>{data[index].name.first} {data[index].name.last}</h1>
+        <h1>{data[index].city}</h1>
+        <h1>{data[index].country}</h1>
+        <h1>{data[index].employer}</h1>
+        <h1>{data[index].title}</h1>
+        <h1>{data[index].favoriteMovies}</h1>
+        <button onClick={previous}>Previous</button>
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>New</button>
+        <button onClick={next}>Next</button>
     </div>
   )
 }
