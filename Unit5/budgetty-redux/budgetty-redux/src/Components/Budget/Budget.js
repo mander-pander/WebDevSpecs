d1a9ee7c716b2f0f9c97b2c1a0bec240a1805be0
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-06-12 13:02:29 -0700

    user data stored in redux store

--- Unit5/budgetty-redux/budgetty-redux/src/Components/Budget/Budget.js
+++ Unit5/budgetty-redux/budgetty-redux/src/Components/Budget/Budget.js
@@ -7,14 +7,21 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import { connect } from 'react-redux';
+import {requestUserData} from './../../redux/userReducer';
 
 
 class Budget extends Component {
 
+  componentDidMount() {
+    this.props.requestUserData();
+  }
+
   render() {
+    const { loading } = this.props.budget;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
           <Nav />
           <div className='content-container'>
@@ -33,4 +40,11 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, { requestUserData })(Budget);
