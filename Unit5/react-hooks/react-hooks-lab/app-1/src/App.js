a83068223c0df373135d2a712e2ffe29f3ea66be
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-06-12 17:40:18 -0700

    useState todo

---
+++ Unit5/react-hooks/react-hooks-lab/app-1/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useState } from 'react';
+import List from './List';
+import AddTodo from './AddTodo';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(item) {
+    const newTodos = [...todos, item];
+    setTodos(newTodos);
+  }
+  return (
+    <div className="App">
+      <AddTodo addTodo={addTodo} />
+      <List list={todos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Unit5/react-hooks/react-hooks-lab/app-1/src/Todo.js
@@ -0,0 +1,5 @@
+const Todo = (props) => {
+    return <h2>{props.item}</h2>
+}
+
+export default Todo;
