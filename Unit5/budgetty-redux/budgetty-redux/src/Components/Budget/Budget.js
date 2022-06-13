f2617ee33b396af654243165cfb2facb33e30e75
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-06-13 09:46:50 -0700

    completed app

--- Unit5/budgetty-redux/budgetty-redux/src/Components/Budget/Budget.js
+++ Unit5/budgetty-redux/budgetty-redux/src/Components/Budget/Budget.js
@@ -8,30 +8,33 @@ import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
 import { connect } from 'react-redux';
-import {requestUserData} from './../../redux/userReducer';
+import { requestUserData } from './../../redux/userReducer';
+import { requestBudgetData, addPurchase, removePurchase } from './../../redux/budgetReducer';
 
 
 class Budget extends Component {
 
   componentDidMount() {
     this.props.requestUserData();
+    this.props.requestBudgetData();
   }
 
   render() {
-    const { loading } = this.props.budget;
+    const { loading, purchases, budgetLimit } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+            <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -47,4 +50,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { requestUserData })(Budget);
+export default connect(mapStateToProps, { requestUserData, requestBudgetData, addPurchase, removePurchase })(Budget);
